ddd46f09b7aa064552ad02f3b670754601ad92db
Michael Woolnough <michael.woolnough@gmail.com>
2025-02-21 19:16:39 +0000

    simplify reader buffering

--- reader.go
+++ reader.go
@@ -2,57 +2,64 @@ package parser
 
 import (
 	"bufio"
-	"bytes"
 	"unicode/utf8"
 )
 
 type readerParser struct {
 	reader *bufio.Reader
-	buf    bytes.Buffer
-	width  int
+	buf    []rune
+	pos    int
 }
 
 func (r *readerParser) next() rune {
+	if r.pos < len(r.buf) {
+		ru := r.buf[r.pos]
+		r.pos++
+
+		return ru
+	}
+
 	ru, s, err := r.reader.ReadRune()
 	if err != nil {
-		r.width = 0
-
-		return -1
+		ru = -1
 	}
 
 	if ru == utf8.RuneError && s == 1 {
-		r.reader.UnreadRune()
-
 		b, _ := r.reader.ReadByte()
 		ru = rune(b)
-
-		r.reader.ReadRune()
 	}
 
-	r.width = s
-	r.buf.WriteRune(ru)
+	r.buf = append(r.buf, ru)
+	r.pos++
 
 	return ru
 }
 
 func (r *readerParser) backup() {
-	if r.width > 0 {
-		r.buf.Truncate(r.buf.Len() - r.width)
-		r.reader.UnreadRune()
-
-		r.width = 0
+	if r.pos > 0 {
+		r.pos--
 	}
 }
 
 func (r *readerParser) get() string {
-	s := r.buf.String()
-	r.width = 0
-
-	r.buf.Reset()
+	s := string(r.buf[:r.pos])
+	r.buf = r.buf[r.pos:]
+	r.pos = 0
 
 	return s
 }
 
 func (r *readerParser) length() int {
-	return r.buf.Len()
+	l := 0
+
+	for _, r := range r.buf[:r.pos] {
+		rl := utf8.RuneLen(r)
+		if rl > 0 {
+			l += rl
+		} else {
+			l++
+		}
+	}
+
+	return l
 }
